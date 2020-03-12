42756d13180f252eaa79a702f0011c6029f6de42
Vitaliy Shapovalov <xflash55@yandex.ru>
2020-03-12 17:20:15 +0300

    install table file and form fiels

---
+++ blocks/helloworld/helloworld_form.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once("{$CFG->libdir}/formslib.php");
+
+class helloworld_form extends moodleform
+{
+    function definition()
+    {
+        $mform =& $this->_form;
+
+        $mform->addElement(
+            'header',
+            'displayinfo',
+            get_string('textfields', 'block_hellowroild'));
+
+        $mform->setType('title', PARAM_RAW);
+
+        $mform->addRule(
+            'title',
+            null,
+            'required',
+            null,
+            'client'
+        );
+
+        $mform->addElement(
+            'htmleditor',
+            'text',
+            get_string('displayedhtml', 'block_helloworld')
+        );
+
+        $mform->addElement(
+            'header',
+            'optional',
+            get_string('optional',
+                'form'),
+            null,
+            false
+        );
+
+        $mform->addElement(
+            'date_time_selector',
+            'displaydate',
+            get_string('displaydate', 'block_helloworld'),
+            array('optional' => true)
+        );
+
+        $mform->setAdvanced('optional');
+        $mform->addElement('hidden', 'blockid');
+        $mform->addElement('hidden', 'courseid');
+        $this->add_action_buttons();
+    }
+}
---
+++ blocks/helloworld/view.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once('../../config.php');
+require_once('helloworld_form.php');
+
+global $DB, $OUTPUT, $PAGE;
+// Check for all required variables.
+$courseid = required_param(
+    'courseid',
+    PARAM_INT
+);
+//breadcrumb
+$blockid = optional_param(
+  'blockid',
+  PARAM_INT
+);
+
+$id = optional_param('id', 0, PARAM_INT);
+
+$settingsnode = $PAGE->settinsnav->add(
+    get_string(
+        'helloworldsettings',
+        'block_helloworld'
+    )
+);
+
+$editurl = new moodle_url(
+    '/block/hello/view.php',
+    array(
+        'id' => $id,
+        'courseid' => $courseid,
+        'blockid' => $blockid
+    )
+);
+
+$editnode = $settingsnode->add(
+    get_string(
+    'editpage',
+    'block_helloworld'
+    ),
+    $editurl
+);
+
+$editnode->make_active();
+
+if(!$course = $DB->get_record('course', array('id'=>$courseid))){
+    print_error('invalidcourse','block_helloworld', $courseid);
+}
+
+require_login($course);
+
+$PAGE->set_url('/blocks/helloworld/view.php', array('id'=> $courseid));
+$PAGE->set_pagelayout('standart');
+$PAGE->set_heading(
+    get_string(
+        'edithtml',
+        'block_helloworld'
+    )
+);
+
+$helloworld = new helloworld_form();
+
+$toform['blockid'] = $blockid;
+$toform['courseid'] = $courseid;
+
+$helloworld->set_data($toform);
+
+if($helloworld->is_cancelled()){
+    // Cancelled forms redirect to the course main page.
+    $couseurl = new moodle_url('/course/view.php', array('id' => $id));
+    redirect($courseurl);
+}else if ($fromform = $helloworld->get_data()){
+    // We need to add code to appropriately act on and store the submitted data
+    // but for now we will just redirect back to the course main page.
+    if(!$DB->insert_record('block_simplehtml', $fromform)){
+        print_error('inserterror', 'block_simplehtml');
+    }
+
+    $courseurl = new moodle_url('/course/view.php', array('id' => $courseid));
+    redirect($courseurl);
+}else{
+    // form didn't validate or this is the first display
+    $site = get_site();
+    echo $OUTPUT->header();
+    $helloworld->display();
+    echo $OUTPUT->footer();
+}
+
+
+
+
+
